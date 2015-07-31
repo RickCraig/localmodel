52db23f40b2c9710b05c01be3647b09c2a304161
Rick Craig <rick@spacerevolver.com>
2015-07-31 15:12:54 +0100

    build

--- dist/js/localmodel.js
+++ dist/js/localmodel.js
@@ -568,6 +568,60 @@ LocalSchema.SchemaTypes = {
 };
 
 /**
+ * Handles the object
+ * @private
+ * @param {Object} data
+ * @param {Object} query
+ * @returns {Boolean} true if there is a match
+ */
+var handleQueryObject = function(data, query) {
+  // Do the business in here for $gte, $gt, $lte, $lt
+
+  if (typeof data === 'number') {
+    var numberMatches = [];
+    if (query.$gte) {
+      numberMatches.push(query.$gte <= data);
+    }
+
+    if (query.$gt) {
+      numberMatches.push(query.$gt < data);
+    }
+
+    if (query.$lte) {
+      numberMatches.push(query.$lte >= data);
+    }
+
+    if (query.$lt) {
+      numberMatches.push(query.$lt > data);
+    }
+
+    return !containsFalse(numberMatches);
+  }
+
+  if (data instanceof Date) {
+    var dateMatches = [];
+
+    if (query.$gte) {
+      dateMatches.push(new Date(query.$gte) <= data);
+    }
+
+    if (query.$gt) {
+      dateMatches.push(new Date(query.$gt) < data);
+    }
+
+    if (query.$lte) {
+      dateMatches.push(new Date(query.$lte) >= data);
+    }
+
+    if (query.$lt) {
+      dateMatches.push(new Date(query.$lt) > data);
+    }
+
+    return !containsFalse(dateMatches);
+  }
+};
+
+/**
  * Takes in a data string and returns
  * true if query matches
  * @private
@@ -589,52 +643,6 @@ var matchQuery = function(data, query) {
     }
 
   if (typeof query === 'object') {
-    // Do the business in here for $gte, $gt, $lte, $lt
-    // Remember to tag this 0.0.2
-
-
-    if (typeof data === 'number') {
-      var numberMatches = [];
-      if (query.$gte) {
-        numberMatches.push(query.$gte <= data);
-      }
-
-      if (query.$gt) {
-        numberMatches.push(query.$gt < data);
-      }
-
-      if (query.$lte) {
-        numberMatches.push(query.$lte >= data);
-      }
-
-      if (query.$lt) {
-        numberMatches.push(query.$lt > data);
-      }
-
-      return !containsFalse(numberMatches);
-    }
-
-    if (data instanceof Date) {
-      var dateMatches = [];
-
-      if (query.$gte) {
-        dateMatches.push(new Date(query.$gte) <= data);
-      }
-
-      if (query.$gt) {
-        dateMatches.push(new Date(query.$gt) < data);
-      }
-
-      if (query.$lte) {
-        dateMatches.push(new Date(query.$lte) >= data);
-      }
-
-      if (query.$lt) {
-        dateMatches.push(new Date(query.$lt) > data);
-      }
-
-      return !containsFalse(dateMatches);
-    }
-
+    return handleQueryObject(data, query);
   }
 };
